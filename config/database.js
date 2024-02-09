6127d7045844a125cf535c619f09cf0a514522c5
júlia moraes <juliademoraess@gmail.com>
2024-02-09 16:58:43 -0300

    chore: basecode

---
+++ config/database.js
@@ -0,0 +1,8 @@
+module.exports = {
+  username: 'root',
+  password: 'pRjc0102',
+  database: 'cadastrodb',
+  host: 'localhost',
+  dialect: 'mysql',
+  port: '3308'
+}
---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express") 
+
+const app = express();
+
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('REQUEST /')
+})
+
+app.listen(3000, () => console.log("Servidor iniciado na porta 3000"));
