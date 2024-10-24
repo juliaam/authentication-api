6bdb659e49fa6d931a48b2f168a4792d3fd3cee4
juliaam <juliademoraess@gmail.com>
2024-10-24 14:27:58 -0300

    fix: swagger routes

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -1,18 +1,24 @@
-import jwt from 'jsonwebtoken'
-
 export default function verifyJWT(req, res, next) {
+  const exceptsPath = ["/api/users/register", "/api/auth/login"];
 
-  const exceptsPath = ['/api/users/register', '/api-docs/', '/api/auth/login'];
+  if (req.path.startsWith("/api-docs")) {
+    return next();
+  }
+
   if (exceptsPath.includes(req.path)) return next();
 
-  const authHeader = req.headers['authorization'];
-  if (!authHeader || !authHeader.startsWith('Bearer ')) return res.status(401).send({ auth: false, message: 'No token provided.' });
-  
+  const authHeader = req.headers["authorization"];
+  if (!authHeader || !authHeader.startsWith("Bearer "))
+    return res.status(401).send({ auth: false, message: "No token provided." });
+
   const token = authHeader.substring(7);
   jwt.verify(token, process.env.SECRET, function (err, decoded) {
-    if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+    if (err)
+      return res
+        .status(500)
+        .send({ auth: false, message: "Failed to authenticate token." });
 
     req.userId = decoded.id;
     next();
-  });w
+  });
 }
--- docs/swagger.js
+++ docs/swagger.js
@@ -1,27 +1,33 @@
-import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerJsdoc from "swagger-jsdoc";
 
 const swaggerDefinition = {
-    info: {
-        title: "API REST de cadastro",
-        description: "Documenta os endpoints do projeto de cadastro",
-        contact: {
-            name: "Júlia Moraes",
-            url: "https://www.linkedin.com/in/júlia-moraes-887673232/",
-            email: "juliademoraess@gmail.com",
-        },
+  info: {
+    title: "API REST de cadastro",
+    description: "Documenta os endpoints do projeto de cadastro",
+    contact: {
+      name: "Júlia Moraes",
+      url: "https://www.linkedin.com/in/júlia-moraes-887673232/",
+      email: "juliademoraess@gmail.com",
     },
-    servers: ["http://localhost:3000"]
-}
-
-
-const swaggerOptions = {
-    swaggerDefinition,
-    apis: ["./app/routes/*.js"]
+  },
+  servers: ["http://localhost:3000"],
+  securityDefinitions: {
+    Bearer: {
+      type: "apiKey",
+      name: "Authorization",
+      in: "header",
+    },
+  },
+  security: [
+    {
+      Bearer: [],
+    },
+  ],
 };
 
+const swaggerOptions = {
+  swaggerDefinition,
+  apis: ["./app/routes/*.js"],
+};
 
 export const specs = swaggerJsdoc(swaggerOptions);
-
-
-
-
